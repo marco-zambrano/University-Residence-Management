6cdbb9bda1c740f9042e713c8c36bf618889a3a3
marco <marcoadrianzambra@gmail.com>
2025-05-28 18:43:48 -0500

    feature(login logic) users json, simulating a DB implementation

--- src/assets/js/login-validation.js
+++ src/assets/js/login-validation.js
@@ -1,12 +1,27 @@
+// JSON with all the credentials (simulating a possible DB implementation) - saving it in local storage
+if (!localStorage.getItem('users')) {
+    localStorage.setItem('users', JSON.stringify([
+        { username: 'admin', password: 'admin123', role: 'admin' },
+        { username: 'user1', password: 'user123', role: 'user' },
+    ]));
+}
+// error message node
+const errorMsg = document.getElementById('errorMsg');
+
+// form submited
 document.getElementById('loginForm').addEventListener('submit', function(e){
     e.preventDefault();
     // inputs values
     const username = this.username.value.trim();
     const password = this.password.value.trim();
-    const errorMsg = document.getElementById('errorMsg');
-    // validating inputs
-    if (!username || !password) {
-        errorMsg.textContent = 'Por favor, completa todos los campos.';
+    
+    // Getting users
+    const users = JSON.parse(localStorage.getItem('users'));
+
+    // finding the user with the credentials introduced -- if user exits, data is correct
+    const user = users.find(u => u.username === username && u.password === password);
+    if (!user) { // user not exits
+        errorMsg.textContent = 'Credenciales incorrectas';
         errorMsg.style.display = 'block';
         return;
     }
