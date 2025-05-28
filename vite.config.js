15d98c7ab7871ea3dd576d9328c42ec779a79796
marco <marcoadrianzambra@gmail.com>
2025-05-28 17:32:44 -0500

    refactor(structure) re-organization and multipages configuration

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    server: {
+        open: 'src/pages/login.html', // Ruta relativa desde src/
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                login: resolve(__dirname, 'src/pages/login.html'),
+                dashboard: resolve(__dirname, 'src/pages/dashboard.html'),
+            },
+        },
+    },
+});
